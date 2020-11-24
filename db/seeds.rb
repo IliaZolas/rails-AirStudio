ddfcad8a2d4b4012960ba21698a48491b39b24b1
Jonas Hermon-Duc <jonas.hermonduc@gmail.com>
2020-11-24 16:57:17 +0100

    seed created for users and studio linked through user ID

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# "title"/"address"/"price"/"description"
+# require 'faker'
+require 'faker'
+puts "cleaning data base"
+
+User.destroy_all
+Studio.destroy_all
+
+puts 'Creating 20 fake users...'
+2.times do
+  user = User.new(
+    email:    Faker::Internet.email,
+    password: "123456"
+  )
+  user.save!
+end
+
+puts 'Creating 20 fake studios...'
+2.times do
+  studio = Studio.new(
+    title:    Faker::Book.title,
+    address: Faker::Address.full_address,
+    price:  rand(85..214),
+    description: Faker::Lorem.paragraphs(number: 1),
+    user_id: User.first.id + 1
+  )
+  studio.save!
+end
+puts 'Finished!'
+
