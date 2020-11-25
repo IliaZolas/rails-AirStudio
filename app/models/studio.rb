4d15042f09294ef85e6224fefa8adc92a3810781
Jonas Hermon-Duc <jonas.hermonduc@gmail.com>
2020-11-25 10:51:00 +0100

    geocoder added

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,7 +1,9 @@
 class Studio < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true, uniqueness: true, length: { in: 3..60 }
+  validates :title, presence: true, uniqueness: true, length: { in: 3.60 }
   validates :address, presence: true
   validates :price, presence: true
   validates :description, presence: true, length: { maximum: 500 }
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address
 end
