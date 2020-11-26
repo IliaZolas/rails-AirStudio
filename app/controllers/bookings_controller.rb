037a691d0afc04a8178563e75100ca649571d67b
Jonas Hermon-Duc <jonas.hermonduc@gmail.com>
2020-11-26 14:39:32 +0100

    booking form new visible, create not done yet

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,13 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @studio = Studio.find(params[:studio_id])
   end
 
   def create
     # @user = user.find(params[:user_id])
     # @studio = studio.find(params[:studio_id])
+    raise
   end
 
   def destroy
